9151a3f199a13996e367bc54322e0bfe66687b0d
HarryPotter1tech <huangziang_2024@qq.com>
2025-12-07 22:14:51 +0800

    添加WS2812b驱动，修了一下编译链

--- cmake/gcc-arm-none-eabi.cmake
+++ cmake/gcc-arm-none-eabi.cmake
@@ -1,23 +1,8 @@
-set(CMAKE_SYSTEM_NAME               Generic)
-set(CMAKE_SYSTEM_PROCESSOR          arm)
-
-set(CMAKE_C_COMPILER_ID GNU)
-set(CMAKE_CXX_COMPILER_ID GNU)
-
-# Some default GCC settings
-# arm-none-eabi- must be part of path environment
-set(TOOLCHAIN_PREFIX                arm-none-eabi-)
-
-set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
-set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++)
-set(CMAKE_LINKER                    ${TOOLCHAIN_PREFIX}g++)
-set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy)
-set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size)
-
-set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".elf")
-set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
-set(CMAKE_EXECUTABLE_SUFFIX_CXX     ".elf")
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_COMPILER "D:/交叉编译gcc/10 2021.10/bin/arm-none-eabi-gcc.exe")
+set(CMAKE_CXX_COMPILER "D:/交叉编译gcc/10 2021.10/bin/arm-none-eabi-g++.exe")
+set(CMAKE_ASM_COMPILER "D:/交叉编译gcc/10 2021.10/bin/arm-none-eabi-gcc.exe")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
